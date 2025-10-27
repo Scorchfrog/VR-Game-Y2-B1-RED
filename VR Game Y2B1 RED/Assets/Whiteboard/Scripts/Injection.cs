fa299686a5bcbd68e0ed9b304ed59a7bcc294961
MatpuX <mato.pullman@gmail.com>
2025-10-27 14:26:52 +0100

    changed some minor things and added stuff to injecting

--- VR Game Y2B1 RED/Assets/Whiteboard/Scripts/Injection.cs
+++ VR Game Y2B1 RED/Assets/Whiteboard/Scripts/Injection.cs
@@ -1,22 +1,32 @@
 using System;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Injection : MonoBehaviour
 {
-    public GameObject drugged;
+    private GameObject drugged;
     public bool dose1 = false;
     public bool dose2 = false;
     public bool dose3 = false;
+    private bool pressed = false;
+    public InputActionReference customButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        customButton.action.started += Drop;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void Drop(InputAction.CallbackContext context)
+    {
+        pressed = !pressed;
+        Debug.Log("boolean is "+ pressed);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -28,9 +38,9 @@ public class Injection : MonoBehaviour
             if (drugged != null)
             {
 
-                if (dose1 == true) { drugged.GetComponent<Drugged>().drug1 = true; Debug.Log("drugged"); }
-                else if (dose2 == true) { drugged.GetComponent<Drugged>().drug2 = true; }
-                else if (dose3 == true) { drugged.GetComponent<Drugged>().drug3 = true; }
+                if (dose1 == true && pressed == true) { drugged.GetComponent<Drugged>().drug1 = true; Debug.Log("drugged"); }
+                else if (dose2 == true && pressed == true) { drugged.GetComponent<Drugged>().drug2 = true; }
+                else if (dose3 == true && pressed == true) { drugged.GetComponent<Drugged>().drug3 = true; }
             }
         }
     }
